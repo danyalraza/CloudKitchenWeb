7e1d920cf8d2f702f7a33af463e1be5cadb87357
Danyal Raza <smdraza@uwaterloo.ca>
2016-02-28 07:34:57 -0500

    add more to routes and services

--- public/js/controllers/SearchCtrl.js
+++ public/js/controllers/SearchCtrl.js
@@ -1,14 +1,14 @@
 angular.module('SearchCtrl', []).controller('SearchController', function($scope, $http) {
-	$http({
-  method: 'GET',
-  url: '/api/favourites'
-}).then(function successCallback(response) {
-	console.log(response.data);
-	$scope.recipes = response.data;
-  }, function errorCallback(response) {
-    console.log(response.body);
-  });
-
-	$scope.tagline = 'Nothing beats a pocket protector!';
-
+	$scope.searchRecipe = function(recipe) {
+		recipe = recipe.replace(" ", "+")
+		$http({
+		method: 'GET',
+		url: '/api/search/' + recipe
+		}).then(function successCallback(response) {
+		console.log(response.data);
+		$scope.recipes = response.data;
+		}, function errorCallback(response) {
+			console.log(response.body);
+		});
+	}
 });
